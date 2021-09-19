0a631e9b15aa380d8af014f7215aaf9ec4e9beca
marcelo <mhosan@gmail.com>
2021-09-19 19:07:15 -0300

    nada

--- start.py
+++ start.py
@@ -1,16 +1,26 @@
 import pymongo
 from pymongo import MongoClient
 
-#conexión
-con = MongoClient('localhost',27017)
+#conexión:
+#con = MongoClient('localhost',27017)  #conexión local
+con = pymongo.MongoClient("mongodb+srv://admin:Ostruca1203@cluster0.rsnsq.mongodb.net/datosprueba?retryWrites=true&w=majority")
 db = con.datosprueba
 
-#coleccion
-tuiteos = db.tuiteos
+#coleccion:
+tuiteos = db.tuiteos1  
 
-resultado = tuiteos.find().count()
+#querys:
+resultado = tuiteos.find({"apporigen":"Twitter for Android"}).limit(1500)
 
 #print ('fecha:', resultado['fecha'],'\n','usuario: ', resultado['usuario'])
 #print (resultado[0]['fecha'], resultado[99]['fecha'])
+#print (resultado)  #el pymongo muestra el cursor (cuando hay mas de un resultado) pero no el contenido...
 #print (f"Fecha: {resultado['fecha']}, Usuario: {resultado['usuario']}")
-print (resultado)
+#print (resultado)
+
+#recorrer un cursor
+for elemento in resultado:
+    print(elemento)
+
+#cerrar la conexión:
+con.close()
